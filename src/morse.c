39fc20d834225b0702401de32aab0a26fedaf2a6
john <john@tuxedo>
2017-10-19 13:29:41 +0100

    Added file header for morse.c

--- src/morse.c
+++ src/morse.c
@@ -1,3 +1,35 @@
+/**
+ * @file morse.c
+ *
+ * @author John Whitmore
+ *
+ * This file contains the implementation of Morse API Functions. This is the 
+ * "good" version of the API where the module does not impose a policy on the 
+ * application code.
+ *
+ * The example is first used in the YouTube video:
+ * "007 - Circular Buffer implementation in C"
+ * (Episode 7 in the cinnamonBun Getting Started series)
+ * https://youtu.be/I8xatqqza6s
+ *
+ * The file is used in subsiquent episodes in the cinnamonBun Getting Started
+ * playlist.
+ *
+ * Copyright 2017 John Whitmore <jwhitmore@electronicsoup.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the version 3 of the GNU General Public License
+ * as published by the Free Software Foundation
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 #include <stdint.h>
 #include <stddef.h>
 
